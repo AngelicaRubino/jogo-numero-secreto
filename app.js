443b8500d8ed0a1700cc60b9cd65d9647877e3d0
Angélica Rubino <110514232+AngelicaRubino@users.noreply.github.com>
2026-05-21 23:56:46 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+alert("Hello, World!");
+let numeroMaximo = 5000;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1
+
+while (chute != numeroSecreto) {
+    chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
+    if (chute === null || chute === "") {
+
+        alert('Ops! Você não digitou nenhum número!');
+    } else if (+chute == numeroSecreto) {
+        break;
+    } else if (+chute > numeroSecreto) {
+        alert(`O número secreto é menor que ${chute}`);
+    } else {
+        alert(`O número secreto é maior que ${chute}`);
+    }
+    tentativas++;
+}
+
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa'
+alert(`Parabéns, você acertou o número secreto ${chute} com ${tentativas} ${palavraTentativa}!`);
